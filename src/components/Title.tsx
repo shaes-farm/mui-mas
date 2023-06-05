a720b44faf031db786127384b44cdc0bdd239e1c
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-06-05 08:19:21 -0400

    Fix typescript problems

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import Typography from '@mui/material/Typography';
+import {Typography} from '@mui/material';
 
 interface TitleProps {
   variant?: "h1"|"h2"|"h3"|"h4"|"h5"|"h6"
-  align?: string
   children?: React.ReactNode
 }
 
 export function Title(props: TitleProps) {
   const { children, variant, ...rest } = props;
   return (
-    /* @ts-ignore */
     <Typography component="h1" variant={variant ?? 'h4'} color="primary" gutterBottom {...rest}>
       {children}
     </Typography>
@@ -20,7 +18,6 @@ export function Title(props: TitleProps) {
 export function SubTitle(props: TitleProps) {
   const { children, variant, ...rest } = props;
   return (
-    /* @ts-ignore */
     <Typography component="h2" variant={variant ?? 'h5'} color="primary" gutterBottom {...rest}>
       {children}
     </Typography>
@@ -30,7 +27,6 @@ export function SubTitle(props: TitleProps) {
 export function Heading(props: TitleProps) {
   const { children, variant, ...rest } = props;
   return (
-    /* @ts-ignore */
     <Typography component="h3" variant={variant ?? 'h6'} color="primary" gutterBottom {...rest}>
       {children}
     </Typography>
