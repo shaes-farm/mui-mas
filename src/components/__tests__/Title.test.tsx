404ef0bf84a8eff419d85dfd96700b4cf1beea51
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-06-05 08:18:47 -0400

    Add tests to title spec

--- src/components/__tests__/Title.test.tsx
+++ src/components/__tests__/Title.test.tsx
@@ -12,14 +12,32 @@ describe('Title component', () => {
     expect(component).not.toBeNull();
   });
   
+  it('should render a Title as a variant', () => {
+    const component = render(<Title variant='h1'/>);
+
+    expect(component).not.toBeNull();
+  });
+  
   it('should render a SubTitle', () => {
     const component = render(<SubTitle />);
 
     expect(component).not.toBeNull();
   });
   
+  it('should render a SubTitle as a variant', () => {
+    const component = render(<SubTitle variant='h2'/>);
+
+    expect(component).not.toBeNull();
+  });
+  
   it('should render a Heading', () => {
     const component = render(<Heading />);
+
+    expect(component).not.toBeNull();
+  });
+  
+  it('should render a Heading as a variant', () => {
+    const component = render(<Heading variant='h3'/>);
 
     expect(component).not.toBeNull();
   });
