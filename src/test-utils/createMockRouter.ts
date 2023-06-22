cad94236d7f6e9d19935bc4d97b2d1d31f078c2b
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-06-22 08:40:04 -0400

    Add test utilities

---
+++ src/test-utils/createMockRouter.ts
@@ -0,0 +1,32 @@
+import { NextRouter } from 'next/router';
+
+export const createMockRouter = (router?: Partial<NextRouter>): NextRouter => {
+  return {
+    basePath: '',
+    pathname: '/',
+    route: '/',
+    query: {},
+    asPath: '/',
+    back: jest.fn(),
+    beforePopState: jest.fn(),
+    forward: jest.fn(),
+    prefetch: jest.fn(() => Promise.resolve()),
+    push: jest.fn(),
+    reload: jest.fn(),
+    replace: jest.fn(),
+    events: {
+      on: jest.fn(),
+      off: jest.fn(),
+      emit: jest.fn(),
+    },
+    isFallback: false,
+    isLocaleDomain: false,
+    isReady: true,
+    defaultLocale: 'en',
+    domainLocales: [],
+    isPreview: false,
+    ...router,
+  };
+};
+
+export default createMockRouter;
