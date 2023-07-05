a66918b360477f526d6347e34b12e39b5f82ce3a
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-07-05 09:02:09 -0400

    Add call to restore mocks

--- src/test-utils/test-setup.ts
+++ src/test-utils/test-setup.ts
@@ -1,7 +1,10 @@
 import "jest-extended";
-
-const Chance = require('chance');
+import Chance from 'chance';
 
 const seed = process.env.CHANCE_SEED ? parseInt(process.env.CHANCE_SEED) : new Chance().natural();
 
 global.chance = new Chance(seed);
+
+afterAll(() => {
+  jest.restoreAllMocks();
+});
