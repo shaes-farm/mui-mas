8bf4ac231ed7a203181454febf121be6e3e7e78f
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-07-22 08:07:30 -0400

    Update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,73 +1,83 @@
 module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended", "plugin:storybook/recommended"],
+  overrides: [{
     env: {
-        browser: true,
-        es2021: true
+      node: true
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended'
-    ],
-    overrides: [
-        {
-            env: {
-                node: true
-            },
-            files: [
-                '.eslintrc.{js,cjs}'
-            ],
-            parserOptions: {
-                sourceType: 'script'
-            }
-        }
-    ],
+    files: ['.eslintrc.{js,cjs}'],
     parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module'
-    },
-    plugins: [
-        '@typescript-eslint',
-        'material-ui'
-    ],
-    rules: {
-    },
-    root: true,
-    settings: {
-        // See: https://github.com/jsx-eslint/eslint-plugin-react#configuration-legacy-eslintrc-
-        react: {
-          createClass: "createReactClass", // Regex for Component Factory to use,
-                                             // default to "createReactClass"
-          pragma: "React",  // Pragma to use, default to "React"
-          fragment: "Fragment",  // Fragment to use (may be a property of <pragma>), default to "Fragment"
-          version: "detect", // React version. "detect" automatically picks the version you have installed.
-                               // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
-                               // It will default to "latest" and warn if missing, and to "detect" in the future
-          flowVersion: "0.53" // Flow version
-        },
-        propWrapperFunctions: [
-            // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
-            "forbidExtraProps",
-            {property: "freeze", object: "Object"},
-            {property: "myFavoriteWrapper"},
-            // for rules that check exact prop wrappers
-            {property: "forbidExtraProps", exact: true}
-        ],
-        componentWrapperFunctions: [
-            // The name of any function used to wrap components, e.g. Mobx `observer` function. If this isn't set, components wrapped by these functions will be skipped.
-            "observer", // `property`
-            {property: "styled"}, // `object` is optional
-            {property: "observer", object: "Mobx"},
-            {property: "observer", object: "<pragma>"} // sets `object` to whatever value `settings.react.pragma` is set to
-        ],
-        formComponents: [
-          // Components used as alternatives to <form> for forms, eg. <Form endpoint={ url } />
-          "CustomForm",
-          {name: "Form", formAttribute: "endpoint"}
-        ],
-        linkComponents: [
-          // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
-          "Hyperlink",
-          {name: "Link", linkAttribute: "to"}
-        ]
+      sourceType: 'script'
     }
-}
+  }],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'material-ui'],
+  rules: {},
+  root: true,
+  settings: {
+    // See: https://github.com/jsx-eslint/eslint-plugin-react#configuration-legacy-eslintrc-
+    react: {
+      createClass: "createReactClass",
+      // Regex for Component Factory to use,
+      // default to "createReactClass"
+      pragma: "React",
+      // Pragma to use, default to "React"
+      fragment: "Fragment",
+      // Fragment to use (may be a property of <pragma>), default to "Fragment"
+      version: "detect",
+      // React version. "detect" automatically picks the version you have installed.
+      // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+      // It will default to "latest" and warn if missing, and to "detect" in the future
+      flowVersion: "0.53" // Flow version
+    },
+
+    propWrapperFunctions: [
+    // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
+    "forbidExtraProps", {
+      property: "freeze",
+      object: "Object"
+    }, {
+      property: "myFavoriteWrapper"
+    },
+    // for rules that check exact prop wrappers
+    {
+      property: "forbidExtraProps",
+      exact: true
+    }],
+    componentWrapperFunctions: [
+    // The name of any function used to wrap components, e.g. Mobx `observer` function. If this isn't set, components wrapped by these functions will be skipped.
+    "observer",
+    // `property`
+    {
+      property: "styled"
+    },
+    // `object` is optional
+    {
+      property: "observer",
+      object: "Mobx"
+    }, {
+      property: "observer",
+      object: "<pragma>"
+    } // sets `object` to whatever value `settings.react.pragma` is set to
+    ],
+
+    formComponents: [
+    // Components used as alternatives to <form> for forms, eg. <Form endpoint={ url } />
+    "CustomForm", {
+      name: "Form",
+      formAttribute: "endpoint"
+    }],
+    linkComponents: [
+    // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
+    "Hyperlink", {
+      name: "Link",
+      linkAttribute: "to"
+    }]
+  }
+};
