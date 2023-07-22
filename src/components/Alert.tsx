3fd171dc1659fa840cee7e3946e0865064aa99de
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-07-22 09:58:16 -0400

    Add documentation

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import MuiAlert from '@mui/material/Alert';
 import type {AlertProps} from '@mui/material/Alert';
 
+/**
+ * An alert displays a short, important message in a way that attracts the user's attention without interrupting the user's task. Pre-styled with an elevation of 6 and a filled variant.
+ */
 export const Alert = React.forwardRef<HTMLDivElement, AlertProps>(
   function Alert(props, ref) {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
