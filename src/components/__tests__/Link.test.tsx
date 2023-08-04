221c3cf83affa4de223d3fde685a523531e4d2eb
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-08-04 08:44:20 -0400

    Update Link unit tests

--- src/components/__tests__/Link.test.tsx
+++ src/components/__tests__/Link.test.tsx
@@ -1,46 +1,60 @@
 import React from 'react';
+import {RouterContext} from 'next/dist/shared/lib/router-context';
 import {cleanup, render} from '@testing-library/react';
+
+import {createMockNextRouter} from '../../test-utils';
 
 import Link from '../Link';
 
-jest.mock('next/router', () => ({
-  useRouter: () => ({
-    pathname: '/bum',
-    push: jest.fn()
-  }),
-}));  
+const router = createMockNextRouter();
 
 describe('Link component', () => {
   afterEach(cleanup);
   
   it('should render an external Link', () => {
-    const url = chance.url();
+    const url = 'https://example.com';
 
-    const component = render(<Link href={url} />);
+    const component = render(
+      <RouterContext.Provider value={router}>
+        <Link href={url} />
+      </RouterContext.Provider>
+    );
 
     expect(component).toBeDefined();
   });
   
   it('should render an external Link without styles', () => {
-    const url = chance.url();
+    const url = 'mailto:user@examples.com';
 
-    const component = render(<Link href={url} noLinkStyle={true} />);
+    const component = render(
+      <RouterContext.Provider value={router}>
+        <Link href={url} noLinkStyle={true} />
+      </RouterContext.Provider>
+    );
 
     expect(component).toBeDefined();
   });
 
   it('should render an internal Link', () => {
-    const url = '/fruity';
+    const url = '/';
 
-    const component = render(<Link href={url} />);
+    const component = render(
+      <RouterContext.Provider value={router}>
+        <Link href={url} />
+      </RouterContext.Provider>
+    );
 
     expect(component).toBeDefined();
   });
 
   it('should render an internal Link without styles', () => {
-    const url = '/fruity';
+    const url = '/';
 
-    const component = render(<Link href={url} noLinkStyle={true} />);
+    const component = render(
+      <RouterContext.Provider value={router}>
+        <Link href={url} noLinkStyle={true} />
+      </RouterContext.Provider>
+    );
 
     expect(component).toBeDefined();
   });
@@ -48,7 +62,11 @@ describe('Link component', () => {
   it('should render a link with legacy behavior', () => {
     const url = chance.url();
 
-    const component = render(<Link href={url} legacyBehavior={true} />);
+    const component = render(
+      <RouterContext.Provider value={router}>
+        <Link href={url} legacyBehavior={true} />
+      </RouterContext.Provider>
+    );
 
     expect(component).toBeDefined();
   });
@@ -56,7 +74,11 @@ describe('Link component', () => {
   it('should render a link without legacy behavior', () => {
     const url = chance.url();
 
-    const component = render(<Link href={url} legacyBehavior={false} />);
+    const component = render(
+      <RouterContext.Provider value={router}>
+        <Link href={url} legacyBehavior={false} />
+      </RouterContext.Provider>
+    );
 
     expect(component).toBeDefined();
   });
@@ -64,13 +86,31 @@ describe('Link component', () => {
   it('should accept a UrlObject as an href', () => {
     const url = new URL(chance.url());
 
-    const component = render(<Link href={url} />);
+    const component = render(
+      <RouterContext.Provider value={router}>
+        <Link href={url} />
+      </RouterContext.Provider>
+    );
 
     expect(component).toBeDefined();
   });
 
   it('should render active pathname', () => {
-    const component = render(<Link href="/bum" activeClassName="bar" />);
+    const component = render(
+      <RouterContext.Provider value={router}>
+        <Link href="/" activeClassName="bar" />
+      </RouterContext.Provider>
+    );
+
+    expect(component).toBeDefined();
+  });
+
+  it('should render link with next.js props', () => {
+    const component = render(
+      <RouterContext.Provider value={router}>
+        <Link href="/" as="/foo" linkAs="/foo" className="active" />
+      </RouterContext.Provider>
+    );
 
     expect(component).toBeDefined();
   });
