2e6aad26ab053f4ed1b7c5ac7884cb4bc876994b
SHAES Farm <60716043+shaes-farm@users.noreply.github.com>
2023-08-21 05:53:03 -0400

    WIP: Adding Supabase session management

--- example/src/pages/_app.tsx
+++ example/src/pages/_app.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import type {AppProps} from 'next/app'
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
 import {createTheme, ThemeProvider} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import {themeOptions} from '@/config/theme';
+import type { Database } from '@/models';
 
-export default function App(props: AppProps) {
+export default function App(props: AppProps<{
+  initialSession: Session,
+}>) {
   const {Component, pageProps} = props;
+  const {initialSession} = pageProps;
+  const [supabase] = React.useState(() => createPagesBrowserClient<Database>());
 
   return (
-    <ThemeProvider theme={createTheme(themeOptions)}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={initialSession}
+    >
+      <ThemeProvider theme={createTheme(themeOptions)}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionContextProvider>
   );
 }
